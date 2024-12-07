1ab99045bfc4959da7c8450883ea47311dd6c0c3
Surabhi <surabhisci2201@gmail.com>
2024-12-07 10:10:50 +0530

    changes in Ball Component (FlatShading)

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -10,7 +10,7 @@ const Ball = (props) => {
     <directionalLight position={[0,0,0.25]}/>
     <mesh castShadow receiveShadow scale={2.75}>
       <icosahedronGeometry args={[1,1]}/>
-      <meshStandardMaterial color='#fff8eb' polygonOffset polygonOffsetFactor={-5}/>
+      <meshStandardMaterial color='#fff8eb' polygonOffset polygonOffsetFactor={-5} flatShading/>
       <Decal position={[0,0,1]} rotation={[2*Math.PI, 0, 6.25]} map={decal}/>
     </mesh>
   </Float>
