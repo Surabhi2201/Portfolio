1b2e2f7cd6f3e481b8ad03850a13d6511e3f8124
Surabhi <surabhisci2201@gmail.com>
2024-12-20 10:32:48 +0530

    tech changes

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import {VerticalTimeline, VerticalTimelineElement} from 'react-vertical-timeline-component';
 import {motion} from 'framer-motion';
 
 import {  technologies} from '../constants';
